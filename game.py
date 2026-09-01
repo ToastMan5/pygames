4ce09e6351f650c2c4d2372ff8d3f6a689bdff79
toastman <joekadocardozo@gmail.com>
2026-09-01 01:43:25 +0100

    OOP

--- game.py
+++ game.py
@@ -9,23 +9,32 @@ SCREEN_WIDTH = 1600
 SCREEN_HEIGHT = 1200
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-player = pygame.Rect((300, 250, 50, 50)) # x coord, y coord, width, height
+rectangleThing = pygame.Rect((300, 250, 50, 50)) # x coord, y coord, width, height
 
+class Player:
+    def __init__(self, health: int, lvl: int, xp: float):
+        self.health = health
+        self.lvl = lvl
+        self.xp = xp
 
+mario = Player(10,1,0)
 
 run = True
 while run: #  GAME LOOP
-    pygame.draw.rect(screen, (255, 0, 0), player)
+    screen.fill((0,0,0))
+
+    pygame.draw.rect(screen, (255, 0, 0), rectangleThing)
 
     key = pygame.key.get_pressed()
     if key[pygame.K_a]: # format is K_<KEY>
-        player.move_ip(-1, 0) # move in place
-    elif key[pygame.K_d]: # format is K_<KEY>
-        player.move_ip(1, 0) # move in place
-    elif key[pygame.K_w]: # format is K_<KEY>
-        player.move_ip(0, -1) # move in place
-    elif key[pygame.K_s]: # format is K_<KEY>
-        player.move_ip(0, 1) # move in place
+        rectangleThing.move_ip(-1, 0) # move in place (left)
+    elif key[pygame.K_d]:
+        rectangleThing.move_ip(1, 0) # move right
+    elif key[pygame.K_w]:
+        rectangleThing.move_ip(0, -1) # move up
+    elif key[pygame.K_s]:
+        rectangleThing.move_ip(0, 1) # move down
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
