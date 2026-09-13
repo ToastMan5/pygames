66abaa935601fb6d627dd8fe1c4925fd944215bc
toastman <joekadocardozo@gmail.com>
2026-09-13 18:54:27 +0100

    a

--- game.py
+++ game.py
@@ -1,46 +1,51 @@
-from importlib.util import source_hash
-from traceback import format_list
-
 import pygame
 
-pygame.init()
 
-SCREEN_WIDTH = 1600
-SCREEN_HEIGHT = 1200
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.init() #initialises pygame
+screen = pygame.display.set_mode((900, 900))
+running = True
+clock = pygame.time.Clock()
+dt=0
 
-rectangleThing = pygame.Rect((300, 250, 50, 50)) # x coord, y coord, width, height
-class Player:
-    def __init__(self, health: int, lvl: int, xp: float):
-        self.health = health
-        self.lvl = lvl
-        self.xp = xp
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+box = (screen.get_width()/3.3,screen.get_height()/1.25,400,40)
+while running:
 
-mario = Player(10,1,0)
-
-print(f"{1 + 1 = }  ")
-
-
-run = False
-while run: #  GAME LOOP
-    screen.fill((0,0,0))
-
-    pygame.draw.rect(screen, (255, 0, 0), rectangleThing)
-
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a]: # format is K_<KEY>
-        rectangleThing.move_ip(-1, 0) # move in place (left)
-    elif key[pygame.K_d]:
-        rectangleThing.move_ip(1, 0) # move right
-    elif key[pygame.K_w]:
-        rectangleThing.move_ip(0, -1) # move up
-    elif key[pygame.K_s]:
-        rectangleThing.move_ip(0, 1) # move down
+    screen.fill("grey") #fill white/clear others
+    pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.rect(screen, "black", box)
 
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    pygame.display.update()
-pygame.quit()
+        if event.type == pygame.QUIT: #user pressed the x button
+            running = False
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        if player_pos.y - 5 < pygame.Vector2(screen.get_width(), screen.get_height())[1] * 0.04:
+            pass
+        else:
+            player_pos.y -= 5
+    if keys[pygame.K_s]:
+        if player_pos.y + 5 > pygame.Vector2(screen.get_width(), screen.get_height())[1] * 0.935:
+            pass
+        else:
+            player_pos.y += 5
+    if keys[pygame.K_a]:
+        if player_pos.x - 5 < pygame.Vector2(screen.get_width(), screen.get_height())[0] * 0.05:
+            pass
+        else:
+            player_pos.x -= 5
+    if keys[pygame.K_d]:
+        if player_pos.x + 5 > pygame.Vector2(screen.get_width(), screen.get_height())[0] * 0.95:
+            pass
+        else:
+            player_pos.x += 5
+    pygame.display.flip() # flip() the display to put your work on screen
+
+    dt = clock.tick(60) / 1000
+    print(f"X{player_pos.y=}")
+    print(f"y{player_pos.x=}")
+
+
+pygame.quit()
