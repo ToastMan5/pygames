74e4f040a27cd0a17fa0aaf3fc5dc7aba4124b63
toastman <joekadocardozo@gmail.com>
2026-09-18 00:23:21 +0100

    delete table2dlist.py

--- table2dlist.py
+++
@@ -1,7 +0,0 @@
-def table2dlist(array, title1, title2):
-    print("-"*20)
-    print(f"{title1}    | {title2}")
-    for row in array:
-        a = row[0]
-        b = row[1]
-        print(f"{a:<8} | {b}")
